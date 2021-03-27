e0717999227d24514991a0532a739ec17bea2caf
Fatrai1 <fatrai.cb@gmail.com>
2021-03-27 22:39:32 +0100

    product.service

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +11,49 @@ export class ProductService {
   apiUrl: string = `http://localhost:3000/products`
 
   constructor(
-    
+    private http: HttpClient,
   ) { }
+
+    list$: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
+    list2$: Observable<Product[]> = this.getAllsum();
+
+    getAllsum(): Observable<Product[]> {
+      return this.http.get<Product[]>(this.apiUrl);
+    }
+
+    getAll(): void {
+      this.http.get<Product[]>(this.apiUrl).subscribe(
+        data => this.list$.next(data)
+      )
+    }
+
+    get(id:number | string): Observable<Product> {
+      id = typeof id === 'string' ? parseInt(id, 10) : id;
+      if(id!=0){
+        return this.http.get<Product>(`${this.apiUrl}/${id}`)
+      }
+      return of(new Product())
+    }
+
+    create(product: Product): void{
+      this.http.post<Product>(`${this.apiUrl}`, product)
+      .subscribe(() => this.getAll()
+      );
+    }
+
+    update(product: Product): void {
+      this.http.patch<Product>(`${this.apiUrl}/${product.id}`, product)
+      .subscribe(() => this.getAll()
+      );
+    }
+
+    remove(product: Product): void{
+      this.http.delete<Product>(`${this.apiUrl}/${product.id}`)
+      .subscribe(() => this.getAll()
+      );
+    }
+
+
+
+
 }
