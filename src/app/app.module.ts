5946e1123296ac4fd74973a0a35a76b1d6021587
Fatrai1 <fatrai.cb@gmail.com>
2021-03-27 15:15:51 +0100

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { InfoCardComponent } from './common/info-card/info-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    DashboardComponent,
+    InfoCardComponent
   ],
   imports: [
     BrowserModule,
