b59e6624397c28e0a0316b27c4e9a8d05cbfa9ab
Fatrai1 <fatrai.cb@gmail.com>
2021-04-09 23:11:55 +0200

    product-editor

--- src/app/page/edit-product/edit-product.component.ts
+++ src/app/page/edit-product/edit-product.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from 'src/app/service/product.service';
+import { Product } from 'src/app/model/product';
+import { switchMap } from 'rxjs/operators';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-product',
@@ -7,9 +13,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProductComponent implements OnInit {
 
-  constructor() { }
+  product$: Observable<Product> = this.activatedRoute.params.pipe(
+    switchMap(params => this.productService.get(params.id))
+  );
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService,
+    private router: Router,
+
+  ) { }
 
   ngOnInit(): void {
   }
 
+  updating: boolean = false;
+
+  onUpdate(form: NgForm, product: Product): void{
+    if(product.id === 0){
+      this.productService.create(product)
+    }else {
+      this.updating = true,
+      this.productService.update(product)
+    }
+     this.router.navigate(['product'])
+     this.productService.getAll()
+     this.productService.getAllsum()
+  }
+  // onShow(): void{
+  //   this.mytoaster.showSuccess()
+  // }
 }
